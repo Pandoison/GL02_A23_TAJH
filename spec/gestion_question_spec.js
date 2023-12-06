0bcce6a75577b26069ae2d337f52069f132c81b6
Antoine <antoine.mazeau31@gmail.com>
2023-12-06 20:30:13 +0100

    test parser OK

--- spec/gestion_question_spec.js
+++
@@ -1,7 +0,0 @@
-describe("Test de la gestion des question", function(){
-
-    it("inti", function(){
-        expect(true).toEqual(true)
-    });
-
-});
---
+++ spec/parser_spec.js
@@ -0,0 +1,30 @@
+describe("Test du parser", function(){
+    const parser = require("../lib/parserGift");
+    const question =require("../data/question")
+    const fs = require("fs")
+    const path = require('path');
+
+    beforeAll(function () {
+        this.fileNameOfQ = "U8-p84-Voc-Linking_words"
+        this.q = new question(
+            "U8 p84 TEST", "I applied for the job. _____ , I didn't get it, unfortunately.",
+            "Look carefully at how the linking expressions are used in context, including the punctuation. Complete the sentences with the linking following words or phrases) <i>although, as well as, despite, however, in order to, so, that's why, too</i>",
+            "MC",
+            [{text: "However", isCorrect: true}]
+        )
+
+    })
+
+    it("Peut ajouter et supprimer une question", function(){
+        let fileContent ;
+
+        parser.addQuestionToGift(this.fileNameOfQ, this.q)
+        fileContent = fs.readFileSync(path.join(__dirname, `../data/gift/${this.fileNameOfQ}.gift`), {encoding: 'utf8', flag: 'r'})
+        expect(fileContent).toContain(this.q.id)
+
+        parser.deleteQuestionFromGift(this.q.id)
+        fileContent = fs.readFileSync(path.join(__dirname, `../data/gift/${this.fileNameOfQ}.gift`), {encoding: 'utf8', flag: 'r'})
+        expect(fileContent).not.toContain(this.q.id)
+    });
+
+});
